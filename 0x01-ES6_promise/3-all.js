170fabdce9f5ad35aedf60de8f70d10dc63b9deb
AshTechs <ashbelh@gmail.com>
2024-06-27 13:33:05 -0700

    Corrected

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,17 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photoResponse, userResponse] = results;
-      const formattedResult = `${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`;
-      console.log(formattedResult);
-      return formattedResult;
-    })
-    .catch((error) => {
-      console.error('Signup system offline');
-      throw error;
-    });
+export default async function handleProfileSignup() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([
+      uploadPhoto(),
+      createUser()
+    ]);
+
+    const formattedResult = `${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`;
+    console.log(formattedResult);
+    return formattedResult;
+  } catch (error) {
+    console.error('Signup system offline');
+    throw error;
+  }
 }
