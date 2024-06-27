2d7b055ad7732e4724a47612554c584a4300a0d5
AshTechs <ashbelh@gmail.com>
2024-06-26 20:58:41 -0700

    Corrected

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,19 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [];
-
-  promises.push(
-    signUpUser(firstName, lastName)
-      .then((data) => ({ status: 'resolved', value: data }))
-      .catch((error) => ({ status: 'rejected', value: error })),
-  );
-
-  promises.push(
-    uploadPhoto(fileName)
-      .then((data) => ({ status: 'resolved', value: data }))
-      .catch((error) => ({ status: 'rejected', value: error })),
-  );
-
-  return Promise.all(promises);
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ])
+    .then((results) => results.map((result) => ({
+      status: result.status === 'fulfilled' ? 'fulfilled' : 'rejected',
+      value: result.status === 'fulfilled' ? result.value : result.reason.message,
+    })))
+    .catch((error) => {
+      console.error('Error in handleProfileSignup:', error);
+      return [];
+    });
 }
