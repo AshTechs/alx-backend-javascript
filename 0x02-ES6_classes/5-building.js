41a02123821c490dc126a102be6a2a42f8cd7730
AshTechs <ashbelh@gmail.com>
2024-06-27 06:55:43 -0700

    Done

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor === Building) {
+      throw new Error('Abstract classes cannot be instantiated directly');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  static evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
