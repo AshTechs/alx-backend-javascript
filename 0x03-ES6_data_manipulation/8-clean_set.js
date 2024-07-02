3b1908ee6fc3c7fb26820a6ed08efa6435c290f0
AshTechs <ashbelh@gmail.com>
2024-07-02 04:38:42 -0700

    Done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,22 @@
 function cleanSet(set, startString) {
-  const filteredValues = Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.substring(startString.length));
+  if (!(set instanceof Set)) {
+    throw new TypeError('Expected a Set as the first argument');
+  }
 
-  return filteredValues.join('-');
+  let result = '';
+  let isFirst = true;
+
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      if (!isFirst) {
+        result += '-';
+      }
+      result += value.substring(startString.length);
+      isFirst = false;
+    }
+  });
+
+  return result;
 }
 
 export default cleanSet;
