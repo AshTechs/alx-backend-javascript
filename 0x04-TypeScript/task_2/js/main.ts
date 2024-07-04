d4f9bbde7b7d2368bf26f65ff56dce835360962b
AshTechs <ashbelh@gmail.com>
2024-07-04 02:47:33 -0700

    Done

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,18 +1,15 @@
-// DirectorInterface
 interface DirectorInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workDirectorTasks(): string;
 }
 
-// TeacherInterface
 interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workTeacherTasks(): string;
 }
 
-// Director class
 class Director implements DirectorInterface {
   workFromHome(): string {
     return 'Working from home';
@@ -27,7 +24,6 @@ class Director implements DirectorInterface {
   }
 }
 
-// Teacher class
 class Teacher implements TeacherInterface {
   workFromHome(): string {
     return 'Cannot work from home';
@@ -42,7 +38,6 @@ class Teacher implements TeacherInterface {
   }
 }
 
-// createEmployee function
 function createEmployee(salary: number | string): Director | Teacher {
   if (typeof salary === 'number' && salary < 500) {
     return new Teacher();
@@ -50,7 +45,6 @@ function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
-// Testing the createEmployee function
-console.log(createEmployee(200)); // Should print Teacher instance
-console.log(createEmployee(1000)); // Should print Director instance
-console.log(createEmployee('$500')); // Should print Director instance
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
