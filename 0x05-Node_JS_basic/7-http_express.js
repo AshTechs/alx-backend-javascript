d9d9e68bb5cc4e9cbdd760d33571ba1666a35207
AshTechs <ashbelh@gmail.com>
2024-08-20 23:43:28 +0000

    Corrected

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const fs = require('fs').promises;
+const app = express();
+const port = 1245;
+
+async function countStudents(path) {
+    try {
+        const data = await fs.readFile(path, 'utf8');
+
+        const lines = data.trim().split('\n').filter(line => line);
+
+        if (lines.length === 0) {
+            return 'Cannot load the database';
+        }
+
+        lines.shift();
+
+        const studentsByField = {};
+
+        lines.forEach((line) => {
+            const student = line.split(',');
+            const firstName = student[0];
+            const field = student[3];
+
+            if (!studentsByField[field]) {
+                studentsByField[field] = [];
+            }
+
+            studentsByField[field].push(firstName);
+        });
+
+        let result = `Number of students: ${lines.length}\n`;
+        for (const [field, students] of Object.entries(studentsByField)) {
+            result += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`;
+        }
+
+        return result;
+
+    } catch (error) {
+        return 'Cannot load the database';
+    }
+}
+
+app.get('/', (_, res) => {
+    res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+    const dbPath = process.argv[2];
+    const studentsInfo = await countStudents(dbPath);
+    res.send(`This is the list of our students\n${studentsInfo}`);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+module.exports = app;
