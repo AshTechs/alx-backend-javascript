16da11598c33dd336aa0c1d124c194c1b252da48
AshTechs <ashbelh@gmail.com>
2024-08-20 21:22:00 +0000

    Done

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+function countStudents(path) {
+    try {
+        const data = fs.readFileSync(path, 'utf8');
+
+        const lines = data.trim().split('\n').filter(line => line);
+
+        if (lines.length === 0) {
+            throw new Error('Cannot load the database');
+        }
+
+        const headers = lines.shift().split(',');
+
+        const studentsByField = {};
+
+        lines.forEach((line) => {
+            const student = line.split(',');
+            const firstName = student[0];
+            const field = student[3];
+
+            if (!studentsByField[field]) {
+                studentsByField[field] = [];
+            }
+
+            studentsByField[field].push(firstName);
+        });
+
+        console.log(`Number of students: ${lines.length}`);
+
+        for (const [field, students] of Object.entries(studentsByField)) {
+            console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+        }
+    } catch (error) {
+        throw new Error('Cannot load the database');
+    }
+}
+
+module.exports = countStudents;
