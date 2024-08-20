07d1ab7d868eea1794bfcf76b69e8344308b30fb
AshTechs <ashbelh@gmail.com>
2024-08-20 22:50:43 +0000

    Corrected

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,34 +4,34 @@ const countStudents = require('./3-read_file_async');
 const port = 1245;
 
 const app = http.createServer(async (req, res) => {
-    if (req.method === 'GET') {
-        if (req.url === '/') {
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end('Hello Holberton School!\n');
-        } else if (req.url === '/students') {
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.write('This is the list of our students\n');
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello Holberton School!\n');
+    } else if (req.url === '/students') {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write('This is the list of our students\n');
 
-            try {
-                await countStudents('database.csv');
-                res.end();
-            } catch (error) {
-                console.error('Error:', error.message);
-                res.write('Cannot load the database\n');
-                res.end();
-            }
-        } else {
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('Not Found\n');
-        }
+      try {
+        await countStudents('database.csv');
+        res.end();
+      } catch (error) {
+        console.error('Error:', error.message);
+        res.write('Cannot load the database\n');
+        res.end();
+      }
     } else {
-        res.writeHead(405, { 'Content-Type': 'text/plain' });
-        res.end('Method Not Allowed\n');
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found\n');
     }
+  } else {
+    res.writeHead(405, { 'Content-Type': 'text/plain' });
+    res.end('Method Not Allowed\n');
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;
