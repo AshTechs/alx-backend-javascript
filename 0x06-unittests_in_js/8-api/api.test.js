480798e80d5c09d35c18dea98141eb1ef84a33a5
AshTechs <66980494+AshTechs@users.noreply.github.com>
2024-08-23 12:33:34 +0000

    Update api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,36 +1,14 @@
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import { app } from './api.js';
+const request = require('request');
+const { expect } = require('chai');
 
-const { expect } = chai;
-chai.use(chaiHttp);
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-describe('Index page', function() {
-  let server;
-
-  before((done) => {
-    server = app.listen(7865, done);
-  });
-
-  after((done) => {
-    server.close(done);
-  });
-
-  it('should return the correct status code', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
-
-  it('should return the correct result', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res.text).to.equal('Welcome to the payment system');
-        done();
-      });
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
   });
 });
